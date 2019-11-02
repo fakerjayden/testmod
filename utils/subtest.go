b686fbad276539292a996dbd3954ae4c45baa168
fakerjayden <13120114366@163.com>
2019-11-02 22:02:04 +0800

    tenth commit: use other github module

--- utils/subtest.go
+++ utils/subtest.go
@@ -1,6 +1,7 @@
 package utils
 
 import "fmt"
+import "github.com/bwangelme/testmod"
 
 func Sub() {
 	fmt.Println("this is the testmod/utils package")
@@ -9,4 +10,5 @@ func Sub() {
 func Tag() {
 	fmt.Println("go mod use tag to downlod ,so new contene --> new tag ,or fail")
 	fmt.Println("你好")
+	fmt.Println(testmod.Hi("赵眼界", "cn"))
 }
