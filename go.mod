11218ddd4cfcc3361f4d8d8a8038850a2fde1cec
fakerjayden <13120114366@163.com>
2019-11-02 22:05:27 +0800

    final ++

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fakerjayden/testmod
 
 go 1.12
+
+require github.com/bwangelme/testmod v1.0.1 // indirect
--- utils/subtest.go
+++ utils/subtest.go
@@ -1,6 +1,8 @@
 package utils
 
 import "fmt"
+
+// 竟然在git push时候会下载并更新go sum
 import "github.com/bwangelme/testmod"
 
 func Sub() {
@@ -10,5 +12,5 @@ func Sub() {
 func Tag() {
 	fmt.Println("go mod use tag to downlod ,so new contene --> new tag ,or fail")
 	fmt.Println("你好")
-	fmt.Println(testmod.Hi("赵眼界", "cn"))
+	fmt.Println(testmod.Hi("赵眼界"))
 }
